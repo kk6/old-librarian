1d96b182150622b24251b7a3558cf868623f6468
kk6 <hiro.ashiya@gmail.com>
2019-01-08 18:10:15 +0900

    Add bocadillo with bulma.

---
+++ librarian/api.py
@@ -0,0 +1,12 @@
+import bocadillo
+
+api = bocadillo.API()
+
+
+@api.route("/")
+async def index(req, res):
+    res.html = await api.template("index.html")
+
+
+if __name__ == '__main__':
+    api.run()
